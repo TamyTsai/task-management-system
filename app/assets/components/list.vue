e63e65b9cf0a45fc19f11ab20254815e5e2177cb
Tamy <tamy8677@gmail.com>
2024-06-24 20:53:41 +0800

    list components, lists index view

---
+++ app/assets/components/list.vue
@@ -0,0 +1,35 @@
+<!-- .vue為vue.js components的副檔名 -->
+
+<template> <!-- 放html一些結構的東西 -->
+    <div class="bg-gray-300 mx-2 w-64 rounded">
+    <!-- <div class="list"> -->
+      <h2 class="px-3 py-1 font-bold">{{ list.name }}</h2>
+      <!-- <h2 class="header">{{ list.name }}</h2> -->
+      <!-- 元件跟局部渲染的檔案一樣，不該主動抓資料，應該要由其他地方餵資料過來 -->
+    </div>
+</template>
+
+<script> // javascript 
+export default {
+    name: 'List', // 可寫可不寫，寫了增加易讀性
+    props: ["list"] // property // 於父層或外層餵資料給元件（list）的手法
+
+    // data: function() { //每個元件 都有自己的 狀態 與 一些資料，所以不能直接在data後接東西，而是要給一個function，然後接該元件 專屬的東西
+    //     return {
+
+    //     }
+    // }
+}
+</script>
+
+<style lang="scss" scoped> /* css */
+/* scoped讓此處寫的css只會生效於此元件，不會污染到外面 */
+ .list {
+    @apply .bg-gray-300 .mx-2 .w-64 .rounded;
+
+    .header {
+    @apply .px-3 .py-1 .font-bold;
+    }
+ }
+
+</style>
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,10 +1,13 @@
 // require jquery
 // require jquery_ujs
 //= require_tree .
+//= vue/dist/vue.esm
+//= components/list
 
 // vue.js
 
 // import Vue from 'vue/dist/vue.esm';
+// import List from 'components/list';
 
 document.addEventListener("turbolinks:load", function(event) {
     let el = document.querySelector('#board');
@@ -18,7 +21,11 @@ document.addEventListener("turbolinks:load", function(event) {
                 // <div id="board" data-lists="<%= @lists.to_json(include: :cards) %>">
                 lists: JSON.parse(el.dataset.lists)
                 // dataset可以抓到html標籤中data屬性中的值，dataset.lists可抓到屬性data-lists中的值
-            }
+            },
+            // components: { List: List } //註冊元件
+            // List名字可以亂取  如{ 'abc': List } 
+            // ES6中key與value相同時，可以只寫一個
+            components: { List }
         });
     }
 })
