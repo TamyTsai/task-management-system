303cd9be8608685c9c14a0f114f7523a5f7f4cba
Tamy <tamy8677@gmail.com>
2024-06-24 19:34:09 +0800

    install vue.js, lists index view and vue.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,20 +1,10 @@
-// This file is automatically compiled by Webpack, along with any other files
-// present in this directory. You're encouraged to place your actual application logic in
-// a relevant structure within app/javascript and only use these pack files to reference
-// that code so it'll be compiled.
-
-import Rails from "@rails/ujs"
-import Turbolinks from "turbolinks"
-import * as ActiveStorage from "@rails/activestorage"
-import "channels"
-
-Rails.start()
-Turbolinks.start()
-ActiveStorage.start()
+// require jquery
+// require jquery_ujs
+//= require_tree .
 
 // vue.js
 
-import Vue from 'vue/dist/vue.esm';
+// import Vue from 'vue/dist/vue.esm';
 
 document.addEventListener("turbolinks:load", function(event) {
     let el = document.querySelector('#board');
@@ -32,4 +22,3 @@ document.addEventListener("turbolinks:load", function(event) {
         });
     }
 })
-
